c8d994af159b8d9de1c776009aab2d0d55cda500
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-02 15:56:16 +0200

    creata tabella con numeri da 1 a 100

--- script.js
+++ script.js
@@ -8,3 +8,15 @@
 // MILESTONE 3
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
+
+// stampiamo i numeri da 1 a 100 
+
+const container = document.querySelector(".container");
+
+for( let i = 1; i <= 100; i++ ) {
+    const div = document.createElement("div");
+
+    div.classList.add("box");
+    div.innerText = i;
+    container.append(div);
+}
